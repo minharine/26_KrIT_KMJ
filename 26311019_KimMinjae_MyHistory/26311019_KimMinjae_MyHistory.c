baf14c4d478f721e47b6bccaa00c6f399b2df3b9
minharine <kkmj2007@gmail.com>
2026-03-25 13:43:10 +0900

    Add files via upload

---
+++ 26311019_KimMinjae_MyHistory/26311019_KimMinjae_MyHistory.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("=======================\n");
+	printf("\t이력서\n");
+	printf("=======================\n");
+	printf("성 명: %s \n", "김민재(1학년)");
+	printf("영 문: %s \n", "KIM MIN JAE");
+	printf("한 자: %s \n", "金民栽");
+	printf("성 별: %s / 생년월일: %s.%s.%s \n", "남", "2007", "01", "14");
+	printf("나 이: %s \n", "만 19");
+	printf("이메일: %s / 전화번호: %s-%s-%s\n", "kkmj2007@gmail.com", "010", "4608", "6121");
+	printf("주소지: %s \n","서울특별시 강동구");
+	printf("학력: %s \n", "고등학교 졸업");
+	printf("자격증: %s", "한자 준5급");
+
+	return 0;
+
+}
